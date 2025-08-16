ceccfcf442f43bc8b56f3d2660479f2a706c7c27
eskender336 <117502882+eskender336@users.noreply.github.com>
2025-08-16 20:25:22 +0500

    Allow blocked addresses to buy and block transfers

--- web3/contracts/mock/SavitriCoin.sol
+++ web3/contracts/mock/SavitriCoin.sol
@@ -8,6 +8,9 @@ contract SavitriCoin is ERC20, Ownable {
     uint256 private constant INITIAL_SUPPLY = 600_000_000;
     bool public transfersEnabled;
     address public saleContract;
+    mapping(address => bool) public blockedAddresses;
+
+    event AddressBlocked(address indexed user, bool blocked);
 
     constructor() ERC20("Savitri Coin", "SAV") {
         _mint(msg.sender, INITIAL_SUPPLY * 10 ** decimals());
@@ -15,6 +18,11 @@ contract SavitriCoin is ERC20, Ownable {
 
     function setSaleContract(address _sale) external onlyOwner {
         saleContract = _sale;
+    }
+
+    function setBlockStatus(address user, bool blocked) external onlyOwner {
+        blockedAddresses[user] = blocked;
+        emit AddressBlocked(user, blocked);
     }
 
     function enableTransfers() external onlyOwner {
@@ -27,6 +35,12 @@ contract SavitriCoin is ERC20, Ownable {
         uint256 amount
     ) internal override {
         super._beforeTokenTransfer(from, to, amount);
+        if (from != address(0)) {
+            require(!blockedAddresses[from], "Address is blocked");
+        }
+        if (to != address(0)) {
+            require(!blockedAddresses[to], "Address is blocked");
+        }
         if (!transfersEnabled) {
             require(
                 from == address(0) ||
